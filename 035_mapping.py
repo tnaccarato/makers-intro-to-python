0cc20a36934e98fdeeb6a6a32b76d650b9f2d735
Tom Naccarato <tcarlnaccarato@gmail.com>
2023-05-04 17:25:44 +0100

    Complete exercise

--- 035_mapping.py
+++ 035_mapping.py
@@ -35,7 +35,11 @@ print("Function: add_one_hundred_to_numbers")
 
 # Return a new list of each number with 100 added
 def add_one_hundred_to_numbers(numbers):
-  pass
+  add_one_hundred = []
+  for num in numbers:
+    add_one_hundred.append(num + 100)
+  return add_one_hundred
+
 
 check_that_these_are_equal(
   add_one_hundred_to_numbers([1, 2, 3, 4]), [101, 102, 103, 104])
