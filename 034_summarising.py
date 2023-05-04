456ad100376e38425c97246357f2bd09ab9b0154
Tom Naccarato <tcarlnaccarato@gmail.com>
2023-05-04 17:23:48 +0100

    Complete task

--- 034_summarising.py
+++ 034_summarising.py
@@ -54,7 +54,10 @@ print("Function: add_up_numbers")
 
 # Add up all the numbers in the list
 def add_up_numbers(numbers):
-  pass
+  total = 0
+  for num in numbers:
+    total += num
+  return total
 
 check_that_these_are_equal(
   add_up_numbers([1, 2, 3, 4]), 10)
